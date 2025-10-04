39935f067de4ecd83e74915ab3ac9d975076fe7e
Andre Paul Cruz <andrepaulcruz29@gmail.com>
2025-10-04 01:03:27 +0000

    Added Validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :genre, presence: true
+  validates :release_year, numericality: { only_integer: true, greater_than: 1800 }
+  validates :director, presence: true
 end
